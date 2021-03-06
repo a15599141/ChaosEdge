e3c761796eb6de233b27bf41698ce871a57a5697
Zijia <451246352@qq.com>
2021-03-06 16:10:14 +0800

    no message

--- ChaosEdge/Assets/ScriptsObj/Ball.cs
+++ ChaosEdge/Assets/ScriptsObj/Ball.cs
@@ -2,33 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ball : MonoBehaviour
 {
     GameObject DiceCopy;
     public int DiceFaceUpNum;
-    GameObject Plane0, Plane1, Plane2, Plane3, Plane4, Plane5, Plane6, Plane7, Plane8, Plane9, Plane10, Plane11;
+    GameObject Planes;
+    public int count;
+    Vector3 newPosition;
     // Start is called before the first frame update
     void Start()
     {
         DiceCopy = GameObject.Find("dice/Dice").gameObject;
-        Plane0 = GameObject.Find("Plane0").gameObject;
-        Plane1 = GameObject.Find("Plane1").gameObject;
-        Plane2 = GameObject.Find("Plane2").gameObject;
-        Plane3 = GameObject.Find("Plane3").gameObject;
-        Plane4 = GameObject.Find("Plane4").gameObject;
-        Plane5 = GameObject.Find("Plane5").gameObject;
-        Plane6 = GameObject.Find("Plane6").gameObject;
-        Plane7 = GameObject.Find("Plane7").gameObject;
-        Plane8 = GameObject.Find("Plane8").gameObject;
-        Plane9 = GameObject.Find("Plane9").gameObject;
-        Plane10 = GameObject.Find("Plane10").gameObject;
-        Plane11 = GameObject.Find("Plane11").gameObject;
+        Planes = GameObject.Find("Planes").gameObject;
+        count = 0;
+        newPosition = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //Vector3 newPosition = new Vector3(1,2,3);
         DiceFaceUpNum = DiceCopy.GetComponent<Dice>().DiceFaceUpNum;
-        float x = Plane4.transform.position.x;
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            
+        }
+         count =  DiceFaceUpNum;
+        newPosition = Planes.transform.GetChild(DiceFaceUpNum).position;
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, newPosition, 0.1f);
+
+
+
     }
 }
--- ChaosEdge/Assets/ScriptsObj/Dice.cs
+++ ChaosEdge/Assets/ScriptsObj/Dice.cs
@@ -5,28 +5,27 @@ using Mirror;
  
 public class Dice : MonoBehaviour
 {
+    GameObject RollButton;
     private int p_x, p_y, p_z;
     private float timer;
     public int DiceFaceUpNum;
     void Start()
     {
-        
+        RollButton = GameObject.Find("Roll").gameObject;
     }
     // Update is called once per frame
-
+    private void OnMouseDown()
+    {
+        //取XYZ的随机旋转值 
+        p_x = Random.Range(1, 30);
+        p_y = Random.Range(1, 30);
+        p_z = Random.Range(1, 30);
+        timer = 0.0f;//点击后计时器清零
+    }
     void Update()
     {
         // dice auto rotate
-        //m_Transform.Rotate(Vector3.forward * 0.7f, Space.Self);
-        if (Input.GetMouseButtonDown(0))
-        {
-            //取XYZ的随机旋转值 
-            p_x = Random.Range(1, 30);
-            p_y = Random.Range(1, 30);
-            p_z = Random.Range(1, 30);
-            timer = 0.0f;//点击后计时器清零
-        }
-        if (timer < 2.0f)//规定 旋转时间
+        if (timer < 3.0f)//规定 旋转时间
         {
             //旋转骰子
             transform.Rotate(new Vector3(transform.rotation.x + p_x, transform.rotation.y + p_y, transform.rotation.z + p_z));
