c03b18bc1c06f925978906a3f0bbb9a445531c28
Zijia <451246352@qq.com>
2021-03-08 20:57:41 +0800

    项目重新上传

---
+++ ChaosEdge/Assets/ScriptSceneChange/ToGameMain.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ToGameMain : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ChaosEdge/Assets/ScriptsObj/testedPlayer.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class testedPlayer : MonoBehaviour
+{
+    GameObject Dice;
+    GameObject Planes;
+    GameObject Roll;
+    public int DiceFaceUpNum;
+    public float checktime;
+    public bool diceIsRotating;
+    public bool playerIsMoving;
+    public float timer;
+    public int planeNum;
+    public int currentRound;
+    public int roundCount;
+    Vector3 lastPosition;
+    Vector3 newPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dice = GameObject.Find("Dice").gameObject;
+        Planes = GameObject.Find("Planes").gameObject;
+        Roll = GameObject.Find("Roll").gameObject;
+        checktime = 0;
+        planeNum = 0;
+        currentRound = 0;
+        playerIsMoving = false;
+        newPosition = new Vector3(0, 0, 0);
+        lastPosition = transform.position;
+    }
+    void checkMoving()
+    {
+        if (Time.time - checktime > 0.1)
+        {
+            checktime = Time.time;
+            //判断是否有移动
+            if ((transform.position - lastPosition).sqrMagnitude > 0.05f)
+            {
+                playerIsMoving = true;
+                //print("is moving");
+            }
+            else
+            {
+                playerIsMoving = false;
+                //print("stop");
+            }
+            lastPosition = transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       checkMoving();
+       diceIsRotating = Roll.GetComponent<Roll>().diceIsRotating;
+       DiceFaceUpNum = Dice.GetComponent<Dice>().DiceFaceUpNum;  // 获取色子点数
+       timer = Roll.GetComponent<Roll>().timer;
+       if (!diceIsRotating && timer != 4.0f) // 如果色子停止旋转且不处于首回合开始前
+       {
+            roundCount = Roll.GetComponent<Roll>().roundCount;
+           if(currentRound!= roundCount)
+           {
+                planeNum = planeNum + DiceFaceUpNum;
+                if(planeNum > 11) planeNum = planeNum - 12;
+                newPosition = Planes.transform.GetChild(planeNum).position; // 取得落点坐标
+                currentRound++;
+           }
+           transform.localPosition = Vector3.MoveTowards(transform.localPosition, newPosition, Time.deltaTime*10); // 每秒10米进行移动
+       }
+       
+
+    }
+}
