58f2f59a630721cc9353d83e9adaf281524b953d
Zijia <451246352@qq.com>
2021-03-10 09:45:03 +0800

    修复玩家只能直线移动至目标点的问题。 修复地图箭头遮挡玩家的问题。 新增回合数记录。

---
+++ Assets/ScriptsObj/RoundText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RoundText : MonoBehaviour
+{
+    GameObject Roll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Roll = GameObject.Find("Roll").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TMP_Text>().text = "Round "+ Roll.GetComponent<Roll>().roundCount;
+    }
+}
