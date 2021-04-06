8b47fc0d59925be88fd9b6416645caeb148c8f56
shawn <shawnzhao233@gmail.com>
2021-04-06 23:26:37 +0800

    修改地图，飞船模型套用（暂时），增加右侧背包，人物ui修改，修改骰子位置。

---
+++ Assets/Scripts/DrawWayLine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawWayLine : MonoBehaviour
+{
+    public Material lineMat; //线段材质 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnPostRender()
+    {
+        GL.PushMatrix();
+        lineMat.SetPass(0);
+
+        RenderPath();
+
+        GL.PopMatrix();
+    }
+
+    public void RenderPath()
+    {
+
+        //生成线
+        for (int i = 0; i < Route.Instacnce.childNodeList.Count; i++)
+        {
+            Vector3 currentPos = Route.Instacnce.childNodeList[i].position;
+            if (i > 0)
+            {
+                Vector3 prevPos = Route.Instacnce.childNodeList[i - 1].position;
+                GL.Begin(GL.LINES);
+                GL.Color(Color.green);
+                GL.Vertex(prevPos);
+                GL.Vertex(currentPos);
+                GL.End();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ShowItemDetail.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowItemDetail : MonoBehaviour
+{
+    public GameObject detailPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowDetailPanel()
+    {
+        detailPanel.SetActive(true);
+    }
+
+    public void HideDetailPanel()
+    {
+        detailPanel.SetActive(false);
+
+    }
+}
